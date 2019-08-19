0f3a492aa197c26f6be1d751e4ba3df17eb5093c
Ambuj Agrawal <ambuj.bits@gmail.com>
2019-08-18 20:52:08 -0700

    Fixed sql queries and pep8 warnings

--- solution.py
+++ solution.py
@@ -5,36 +5,39 @@ DBNAME = "news"
 
 
 def get_results(query):
-    db = psycopg2.connect(database=DBNAME)
+    try:
+        db = psycopg2.connect(database=DBNAME)
+    except Exception as e:
+        print("Encountered following error in connecting to database: %s" % e)
+        return
     c = db.cursor()
     c.execute(query)
     results = c.fetchall()
     db.close()
     return results
 
+
 if __name__ == '__main__':
-    query1 = "select substring(path,10,100) as article, count (*) as views \
-                from log where path like '/article/%' group by path order\
-                by views desc limit 3;"
-    query2 = "select name, count(*) from (select substring(path,10,100) as article, author \
+    query1 = "select slug as article, count (*) as views \
+                from log join articles on substring(path,10,100) = slug\
+                group by slug order by views desc limit 3;"
+    query2 = "select name, count(*) from (select slug as article, author \
                 from log join articles on substring(path,10,100) = slug) \
                 as subq join authors on authors.id=subq.author \
                 group by name order by count desc;"
-    query3 = "select a.day, cast(a.errors*100 as real)/cast(b.total_queries \
-                as real) as error_percent from (select cast(time as date) \
+    query3 = "select a.day, round((a.errors*100.0)/b.total_queries,2) as \
+                error_percent from (select cast(time as date) \
                 as day,count (*) as errors from log where status!='200 OK' \
                 group by day) as a join (select cast(time as date) as day,\
                 count (*) as total_queries from log group by day) as b \
-                on a.day=b.day where cast(a.errors*100 as real)/cast\
-                (b.total_queries as real)>1;"
+                on a.day=b.day where (a.errors*100.0)/b.total_queries>1;"
 
     print("\n1. What are the most popular three articles of all time? ")
-    for record in get_results(query1):
-        print(str(record[0])+" with "+str(record[1])+" views")
+    for article, views in get_results(query1):
+        print("{} with {} views".format(article, views))
     print("\n2.Who are the most popular article authors of all time?")
-    for record in get_results(query2):
-        print(str(record[0])+" with "+str(record[1])+" views")
+    for author, views in get_results(query2):
+        print("{} with {} views".format(author, views))
     print("\n3. On which days did more than 1% of requests lead to errors?")
-    for record in get_results(query3):
-        print("On "+str(record[0]) +
-              " there were "+str(round(record[1], 2))+"% errors")
+    for date, errors in get_results(query3):
+        print("On {} there were {}% errors".format(date, errors))
